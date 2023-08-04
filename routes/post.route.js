ea6eac27b5276e55a279e48e777afd41d390154e
Bernardus Joshua Otto Prayoga <bernarduuuus@gmail.com>
2023-08-04 18:32:48 +0700

    remove post detail

--- routes/post.route.js
+++ routes/post.route.js
@@ -15,18 +15,18 @@ router.get("/", async (req, res) => {
     });
 });
 
-router.get("/:id", m.mustBeInteger, async (req, res) => {
-    const id = req.params.id
-    await post.getPost(id)
-    .then(post => res.json(post))
-    .catch(err => {
-        if (err.status) {
-            res.status(err.status).json({ message: err.message });
-        } else {
-            res.status(500).json({ message: err.message });
-        }
-    });
-});
+// router.get("/:id", m.mustBeInteger, async (req, res) => {
+//     const id = req.params.id
+//     await post.getPost(id)
+//     .then(post => res.json(post))
+//     .catch(err => {
+//         if (err.status) {
+//             res.status(err.status).json({ message: err.message });
+//         } else {
+//             res.status(500).json({ message: err.message });
+//         }
+//     });
+// });
 
 router.post('/', m.checkFieldsPosts, async (req, res) => {
     await post.insertPost(req.body)
